5911087db563c3da7980680958232993fcbedc8f
yiyanglin0102 <yiyanglin0102@gmail.com>
2023-10-17 01:28:21 -0400

    Added Netlify functions

---
+++ public/app.js
@@ -0,0 +1,18 @@
+const questionElement = document.getElementById('question');
+const answerElement = document.getElementById('answer');
+const askButton = document.getElementById('askButton');
+
+askButton.addEventListener('click', async () => {
+    const question = questionElement.value;
+
+    const response = await fetch('/.netlify/functions/ask-openai', {
+        method: 'POST',
+        body: JSON.stringify({ question }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const data = await response.json();
+    answerElement.innerText = data.answer;
+});
