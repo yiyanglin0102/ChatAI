ee9af0eaa4e7f7b73527032e757953ff3c0e168d
yiyanglin0102 <yiyanglin0102@gmail.com>
2023-10-17 01:55:57 -0400

    Added return function and mobile view

--- public/app.js
+++ public/app.js
@@ -1,18 +1,32 @@
-const questionElement = document.getElementById('question');
-const answerElement = document.getElementById('answer');
-const askButton = document.getElementById('askButton');
+const chatBox = document.getElementById('chat-box');
+const userInput = document.getElementById('user-input');
+const sendButton = document.getElementById('send-button');
+const chatForm = document.getElementById('chat-form');
 
-askButton.addEventListener('click', async () => {
-    const question = questionElement.value;
+async function sendMessage() {
+    const question = userInput.value;
+    if (!question.trim()) return; // Prevent sending empty messages
+    userInput.value = '';
+    chatBox.innerHTML += `<div class="user-message">${question}</div>`;
 
     const response = await fetch('/.netlify/functions/askOpenAI', {
         method: 'POST',
-        body: JSON.stringify({ question }),
+        body: JSON.stringify({ question: question }),
         headers: {
-            'Content-Type': 'application/json'
-        }
+            'Content-Type': 'application/json',
+        },
     });
-
     const data = await response.json();
-    answerElement.innerText = data.answer;
+
+    chatBox.innerHTML += `<div class="assistant-message">${data.answer}</div>`;
+}
+
+sendButton.addEventListener('click', async (e) => {
+    e.preventDefault(); // Prevent form submission when button clicked
+    sendMessage();
+});
+
+chatForm.addEventListener('submit', async (e) => {
+    e.preventDefault(); // Prevent default form submission on enter
+    sendMessage();
 });
